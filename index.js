73770fde1cb1b280614be4c34d403ed52874f7d9
Sapo <sapo@gmail.com>
2026-03-11 14:01:12 -0500

    se agrega index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-'use strict';
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer(function(req, res){
-res.writeHead(200, {'content-type': 'text/plain' });
-res.end('Hola mundo');
-});
-server.listen(5000);
+app.get('/prueba',(req, res)=> {
+    res.send('Hola Camilo 2')
+})
+
+app.listen(port,()=>{
+    console.log('La aplicacion se esta ejecutando por el puerto ')
+})
