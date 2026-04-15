43372d49f5366227b29a560496e54472639912fc
mango <mango@gmail.com>
2026-04-15 12:40:40 -0500

    se guarda cambios

--- servers.js
+++ servers.js
@@ -9,11 +9,21 @@ const app = express();
 app.use(cors()); // Permitir solicitudes CORS
 app.use(express.json()); // Permitir recibir datos JSON en el cuerpo de la solicitud
 
-// Conectar a MongoDB
-mongoose.connect(process.env.MONGO_URI) .then(() => console.log("MongoDB conectado")) .catch(err => console.error(err)); // Rutas
-app.use("/api/animals", require("./routes/animalRoutes")); // Ruta base
-app.get("/", (req, res) => {
+// Conectar a MongoDB 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB conectado")) 
+.catch(err => console.error(err));
 
-res.send("API funcionando 🚀"); }); // Puerto de conexión
-const PORT = process.env.PORT || 3000; app.listen(PORT, () => { console.log(`Servidor corriendo en el puerto ${PORT}`); });
-await conectionDatabase();
+// Rutas 
+app.use("/api/animals", require("./routes/animalRoutes"));
+// Ruta base 
+app.get("/", (req, res) => {
+    res.send("API funcionando 🚀");
+});
+
+// Puerto de conexión 
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => { 
+    console.log(`Servidor corriendo en el puerto ${PORT}`); 
+});
+
